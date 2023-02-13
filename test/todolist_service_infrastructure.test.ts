957e6c24064b77bf6fbc18911efbacc29d748e0f
Jerry Zhang <jerryzhang@jerrys-air.lan>
2023-02-12 18:55:29 -0800

    Initial commit

---
+++ test/todolist_service_infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TodolistServiceInfrastructure from '../lib/todolist_service_infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/todolist_service_infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TodolistServiceInfrastructure.TodolistServiceInfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
